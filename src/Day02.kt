f5136e4488faeb89587087174dafefb5985c9081
Timo Dankert <tdankert@slashwhy.de>
2021-12-02 10:10:20 +0100

    Formatting

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,12 +1,11 @@
-
 fun main() {
 
     fun part1(input: List<String>): Pair<Int, Int> {
         var position = Pair(0,0);
 
         for (i in input) {
-            var line = i.split(" ")
-            if (line[0].equals("forward")) {
+            val line = i.split(" ")
+            if (line[0] == ("forward")) {
                 position = Pair(position.first + line[1].toInt(), position.second)
             } else if (line[0] == "up") {
                 position = Pair(position.first, position.second - line[1].toInt())
@@ -23,15 +22,14 @@ fun main() {
         var position = Pair(0,0);
         var aim = 0
         for (i in input) {
-            var line = i.split(" ")
-            if (line[0].equals("forward")) {
-                position = Pair(position.first + line[1].toInt(), position.second - (line[1].toInt() * aim))
+            val line = i.split(" ")
+            if (line[0] == ("forward")) {
+                Pair(position.first + line[1].toInt(), position.second - (line[1].toInt() * aim)).also { position = it }
             } else if (line[0] == "up") {
                 aim += line[1].toInt()
             } else if (line[0] == "down") {
                 aim -= line[1].toInt()
             }
-
         }
 
         println(position.first * position.second)
