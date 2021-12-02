5d5ae4e1d28b93a163be9faea7bf29cd45f302cb
Timo Dankert <tdankert@slashwhy.de>
2021-12-02 16:17:41 +0100

    Added test class

---
+++ src/test/kotlin/Day01_Test.kt
@@ -0,0 +1,27 @@
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class Day01_Test {
+    private val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
+    var d = Day01()
+
+    @Test
+    fun part1() {
+        val expected = 7
+
+        var actual = d.part1(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun part2() {
+        val expected  = 5
+
+        var actual = d.part2(input)
+
+        assertEquals(expected, actual)
+    }
+}
