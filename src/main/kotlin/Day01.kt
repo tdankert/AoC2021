bc1c198fb6e0db995d4d6adac452d4396ba7b85b
Timo Dankert <tdankert@slashwhy.de>
2021-12-02 16:17:23 +0100

    Refactoring

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,40 +1,42 @@
-
-    fun main() {
-
-        fun part1(input: List<Int>): Int {
-            var counter = 0
-            for (i in input.indices) {
-                if (i > 0 && input[i] > input[i-1]) {
-                    counter += 1
-                }
+class Day01 {
+    fun part1(input: List<Int>): Int {
+        var counter = 0
+        for (i in input.indices) {
+            if (i > 0 && input[i] > input[i-1]) {
+                counter += 1
             }
-            return counter
         }
-
-        fun part2(input: List<Int>): Int {
-
-            fun sum(index: Int): Int {
-                var output = input[index]
-                if (index > 0)
-                    output += input[index-1]
-                if (index < input.size - 1)
-                    output += input[index+1]
-
-                return output
-            }
-
-            var counter = 0
-            for (i in 2..input.size-2) {
-
-                if(sum(i) > sum(i-1)) {
-                    counter += 1
-                }
-            }
-
-            return counter
-        }
-
-        val input = readIntInput("Day01_test")
-        println(part1(input))
-        println(part2(input))
+        return counter
     }
+
+    fun part2(input: List<Int>): Int {
+
+        fun sum(index: Int): Int {
+            var output = input[index]
+            if (index > 0)
+                output += input[index-1]
+            if (index < input.size - 1)
+                output += input[index+1]
+
+            return output
+        }
+
+        var counter = 0
+        for (i in 2..input.size-2) {
+
+            if(sum(i) > sum(i-1)) {
+                counter += 1
+            }
+        }
+
+        return counter
+    }
+
+}
+
+fun main() {
+    val day = Day01()
+    val input = readIntInput("Day01")
+    println(day.part1(input))
+    println(day.part2(input))
+}
