433e4449ae79e305631b7b3dcf1b9f68a0acac6f
Timo Dankert <timo.dankert@posteo.net>
2021-12-03 09:21:00 +0100

    added test for day 3

---
+++ src/test/kotlin/Day03Test.kt
@@ -0,0 +1,39 @@
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class Day03Test {
+    private val input =
+        listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")
+
+    var d = Day03()
+
+    @Test
+    fun part1() {
+        val expected = 198
+
+        val actual = d.part1(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun part2() {
+        val expected  = 900
+
+        val actual = d.part2(input)
+
+        assertEquals(expected, actual)
+    }
+}
