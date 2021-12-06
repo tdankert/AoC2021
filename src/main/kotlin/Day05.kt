e0e5b1ceea2a2c60291415e2c8dc16d6d8d89fef
Timo Dankert <tdankert@slashwhy.de>
2021-12-06 15:44:45 +0100

    Day 5 skeleton

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,21 @@
+import java.math.BigInteger
+
+class Day05 {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val day = Day05()
+    var input = readInput("Day05")
+
+    day.part1(input)
+    day.part2(input)
+
+}
