3ee506103ef496488dd547f62553b4fa7f66346d
Timo Dankert <tdankert@slashwhy.de>
2021-12-08 11:00:31 +0100

    Part 2

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,5 +1,3 @@
-import kotlin.math.sqrt
-
 class Day08 {
 
     fun part1(input: List<String>): Int {
@@ -17,12 +15,61 @@ class Day08 {
         return counter
     }
 
-    /**
-     * a very naive approach
-     */
     fun part2(input: List<String>): Int {
-        return 0
+        fun digsIn(a: String, b: String) : Int {
+            var counter = 0
+            for (c in a) {
+                if (!b.contains(c)) {
+                    counter += 1
+                }
+            }
+            return counter
+        }
+
+        var result = 0
+        for (entry in input) {
+            val learn = entry.split("|").first().trim().split(" ")
+
+            var digs = MutableList<String>(10) {""}
+
+            // Find digits by size
+            digs[1] = learn.find { x -> x.length == 2 }.toString().alphabetized()
+            digs[4] = learn.find { x -> x.length == 4 }.toString().alphabetized()
+            digs[7] = learn.find { x -> x.length == 3 }.toString().alphabetized()
+            digs[8] = learn.find { x -> x.length == 7 }.toString().alphabetized()
+
+            // Find 6 (length 6 and one segment of 1 is not in it
+            digs[6] = learn.find { x -> x.length == 6 && digsIn(digs[1],x.alphabetized()) == 1}.toString().alphabetized()
+            digs[0] = learn.find { x -> x.length == 6 && digsIn(digs[4],x.alphabetized() ) == 1 && x.alphabetized() != digs[6] }.toString().alphabetized()
+            digs[9] = learn.find { x -> x.length == 6 && x.alphabetized() !=  digs[0] && x.alphabetized() != digs[6] }.toString().alphabetized()
+
+
+            digs[5] = learn.find { x -> x.length == 5 && digsIn(x.alphabetized(), digs[6]) == 0}.toString().alphabetized()
+            digs[3] = learn.find { x -> x.length == 5 && digsIn(x.alphabetized(), digs[9]) == 0 && x.alphabetized() != digs[5] }.toString().alphabetized()
+            digs[2] = learn.find { x -> x.length == 5 && x.alphabetized() !=  digs[3] && x.alphabetized() != digs[5] }.toString().alphabetized()
+
+            // Find 0
+
+
+
+
+//            println(digs)
+            var digits = entry.split("|").last().trim().split(" ")
+
+            var value = ""
+            for (digit in digits) {
+                for (i in 0..9) {
+                    if (digs[i] == digit.alphabetized()) {
+                        value += i.toString()
+                    }
+                }
+            }
+            result += value.toInt()
+            println(value.toInt())
+        }
+        return result
     }
+
 }
 
 fun main() {
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,4 @@
+import java.awt.Point
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
@@ -16,3 +17,50 @@ fun readIntInputOneLine(name: String) =
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun String.alphabetized() = String(toCharArray().apply { sort() })
+
+class point(x: Int, y: Int) {
+    var x = 0
+    var y = 0
+
+
+    fun equals(point: point): Boolean {
+        if (this.x == point.x && this.y == point.y) {
+            return true
+        }
+        return false
+    }
+
+    fun stepTo(point: point): List<point>{
+        var result = mutableListOf<point>()
+        if (this.x == point.x) {
+            if (this.y < point.y) {
+                // step upward
+                for (dy in this.y..point.y) {
+                    result.add(point(x, dy))
+                }
+            } else {
+                // step downward
+                for (dy in this.y downTo point.y) {
+                    result.add(point(x, dy))
+                }
+            }
+        } else if (this.y == point.y){
+            if (this.x < point.x) {
+                // step upward
+                for (dx in this.x..point.x) {
+                    result.add(point(dx, y))
+                }
+            } else {
+                // step downward
+                for (dx in this.x downTo point.x) {
+                    result.add(point(dx, y))
+                }
+            }
+        }
+
+
+        return result
+    }
+}
