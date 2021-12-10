cbdef8cd5ea155a03cc87953f83de35b05f22e09
Timo Dankert <timo.dankert@posteo.net>
2021-12-10 09:33:20 +0100

    Day 10 pt 2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,3 +1,5 @@
+import java.math.BigInteger
+
 class Day10 {
 
     private val roundPoints = 3
@@ -5,42 +7,44 @@ class Day10 {
     private val curlyPoints = 1197
     private val anglePoints = 25137
 
-    fun part1(input: List<String>): Int {
 
-        fun parseLine(line: String) : Char {
-            var list = mutableListOf<Char>()
-            for (c in line) {
-                when (c) {
-                    '(' -> list.add(c)
-                    '[' -> list.add(c)
-                    '{' -> list.add(c)
-                    '<' -> list.add(c)
+    private fun parseLine(line: String) : Char {
+        var list = mutableListOf<Char>()
+        for (c in line) {
+            when (c) {
+                '(' -> list.add(c)
+                '[' -> list.add(c)
+                '{' -> list.add(c)
+                '<' -> list.add(c)
 
-                    ')' -> {
-                        if (list.removeLast() != '(') {
-                            return c
-                        }
-                    }
-                    ']' -> {
-                        if (list.removeLast() != '[') {
-                            return c
-                        }
-                    }
-                    '}' -> {
-                        if (list.removeLast() != '{') {
-                            return c
-                        }
-                    }
-                    '>' -> {
-                        if (list.removeLast() != '<') {
-                            return c
-                        }
+                ')' -> {
+                    if (list.removeLast() != '(') {
+                        return c
                     }
                 }
-
+                ']' -> {
+                    if (list.removeLast() != '[') {
+                        return c
+                    }
+                }
+                '}' -> {
+                    if (list.removeLast() != '{') {
+                        return c
+                    }
+                }
+                '>' -> {
+                    if (list.removeLast() != '<') {
+                        return c
+                    }
+                }
             }
-            return ' '
+
         }
+        return ' '
+    }
+
+    fun part1(input: List<String>): Int {
+
 
         var errorScore = 0
 
@@ -57,8 +61,50 @@ class Day10 {
         return errorScore
     }
 
-    fun part2(input: Array<IntArray>): Int {
-        return 0
+    private fun autoComplete(line: String) : BigInteger {
+        val list = mutableListOf<Char>()
+        for (c in line) {
+            when (c) {
+                '(' -> list.add(c)
+                '[' -> list.add(c)
+                '{' -> list.add(c)
+                '<' -> list.add(c)
+                ')' -> list.removeLast()
+                ']' -> list.removeLast()
+                '}' -> list.removeLast()
+                '>' -> list.removeLast()
+            }
+        }
+
+        var score = BigInteger.ZERO
+        for (c in list.reversed()) {
+            score *= BigInteger.valueOf(5)
+            when(c) {
+                '(' -> score += BigInteger.ONE
+                '[' -> score += BigInteger.valueOf(2)
+                '{' -> score += BigInteger.valueOf(3)
+                '<' -> score += BigInteger.valueOf(4)
+            }
+        }
+
+        return score
+    }
+
+    fun part2(input: List<String>): BigInteger {
+        var incompleteInput = mutableListOf<String>()
+        for(line in input) {
+            if (parseLine(line) == ' ')
+                incompleteInput.add(line)
+        }
+
+        val scores = mutableListOf<BigInteger>()
+
+        for (line in incompleteInput) {
+            scores.add(autoComplete(line))
+        }
+
+        val index = (scores.size - 1) / 2
+        return scores.sorted()[index]
     }
 
 }
@@ -67,6 +113,6 @@ fun main() {
     val day = Day10()
     val inp = readInput("Day10")
     println(day.part1(inp))
-//    println(day.part2(inp))
+    println(day.part2(inp))
 
 }
