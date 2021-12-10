6c643125496f84702808d4c5dd38b1b50ba1996d
Timo Dankert <timo.dankert@posteo.net>
2021-12-10 09:32:14 +0100

    Test pt 2

--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,5 +1,6 @@
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import java.math.BigInteger
 
 class Day10Test {
     val input = readInput("Day10test")
@@ -15,12 +16,12 @@ class Day10Test {
         assertEquals(expected, actual)
     }
 
-//    @Test
-//    fun part2() {
-//        val expected  =
-//
-//        val actual = d.part2(input)
-//
-//        assertEquals(expected, actual)
-//    }
+    @Test
+    fun part2() {
+        val expected  = BigInteger.valueOf(288957)
+
+        val actual = d.part2(input)
+
+        assertEquals(expected, actual)
+    }
 }
