693c3df1cc16d14d1021173ba4af49542a22fbe1
Timo Dankert <timo.dankert@posteo.net>
2021-12-11 09:16:51 +0100

    Day 11 skeleton

---
+++ src/main/kotlin/Day11.kt
@@ -0,0 +1,20 @@
+import java.math.BigInteger
+
+class Day11 {
+    fun part1(input: Array<IntArray>): Int {
+        return 0
+    }
+
+     fun part2(input: Array<IntArray>): Int {
+        return 0
+    }
+
+}
+
+fun main() {
+    val day = Day11()
+    val inp = readIntArray("Day11")
+    println(day.part1(inp))
+    println(day.part2(inp))
+
+}
